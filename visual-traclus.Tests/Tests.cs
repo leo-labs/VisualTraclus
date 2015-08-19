b237245ee5032c3fc7c3b33e4b67bfdf46124658
Leonard Kleinhans <leonard.kleinhans42@gmail.com>
2015-08-19 15:02:35 +0200

    Initital solution

---
+++ visual-traclus.Tests/Tests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using Should;
+
+namespace visual-traclus.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Should_be_true()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
